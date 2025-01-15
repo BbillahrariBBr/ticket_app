80954b0f9eccbff2b7a09f4d5606924c3923b41c
BbillahrariBBr <mb.billah8@gmail.com>
2025-01-15 15:20:52 +0600

    Add fluentui_icons 10.40.44

--- lib/base/bottom_nav_bar.dart
+++ lib/base/bottom_nav_bar.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -18,12 +19,26 @@ class _BottomNavBarState extends State<BottomNavBar> {
       body: Center(
         child: Text("Ticket Info"),
       ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.mail), label: "Mail"),
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings")
-
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.blueGrey,
+          unselectedItemColor: Color(0xFF526400),
+          showSelectedLabels: false,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+                label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+                label: "Search"),
+            BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled)
+                , label: "Tickets"),
+            BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled)
+                , label: "Profile"),
+          ]),
     );
   }
 }
